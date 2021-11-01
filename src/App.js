9e3485c9a61bcbe20da164314c136fcda08b392e
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-01 13:22:42 -0400

    Setup basic page routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <h1>I am the Home Page</h1>
+        </Route>
+        <Route path='/browse'>
+          <h1>I am the Browse Page</h1>
+        </Route>
+        <Route path='/signin'>
+          <h1>I am the Signin Page</h1>
+        </Route>
+        <Route path='/signup'>
+          <h1>I am the Signup Page</h1>
+        </Route>
+        <Route path='/profile'>
+          <h1>I am the Profile Page</h1>
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
