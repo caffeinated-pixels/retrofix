4e8372a6a7a41c433232624b9b15c75cb15a86f3
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-01 16:27:42 -0400

    Add images folder to public

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>I am the Home Page</h1>
+  return (
+    <>
+      <h1>I am the Home Page</h1>
+      <img src='./images/misc/logo.svg' alt='Netflix logo' />
+    </>
+  )
 }
