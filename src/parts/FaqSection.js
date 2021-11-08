7030db56d00e5142e9a5e034ecdfd4dabee68e3e
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-08 14:20:18 -0500

    Increase faq title size at 500px

--- src/parts/FaqSection.js
+++ src/parts/FaqSection.js
@@ -14,6 +14,10 @@ const Title = styled.h2`
   font-size: 1.625rem;
   line-height: 1.15;
   padding: 0 5%;
+
+  @media (min-width: 550px) {
+    font-size: 2.5rem;
+  }
 `
 
 const AccordionWrapper = styled.div`
