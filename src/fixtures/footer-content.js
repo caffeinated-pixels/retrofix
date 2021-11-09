6ac3d7beb9d247002218b6ed6f69950e85295a97
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-09 09:39:00 -0500

    Reaorder footer text to work with grid system

--- src/fixtures/footer-content.js
+++ src/fixtures/footer-content.js
@@ -2,21 +2,48 @@ export const footerHomeContent = {
   title: 'Questions? Call 1-555-123-4567',
   body: [
     'FAQ',
-    'Account',
-    'Investor Relations',
-    'Redeem Gift Cards',
-    'Ways to Watch',
-    'Privacy',
-    'Corporate Information',
-    'Speed Test',
-    'Only on Netflix',
     'Help Center',
+    'Account',
     'Media Center',
+
+    'Investor Relations',
     'Jobs',
+    'Redeem Gift Cards',
     'Buy Gift Cards',
+
+    'Ways to Watch',
     'Terms of Use',
+    'Privacy',
     'Cookie Preferences',
+
+    'Corporate Information',
     'Contact Us',
+    'Speed Test',
     'Legal Notices',
+
+    'Only on Netflix',
   ],
 }
+
+// export const footerHomeContent = {
+//   title: 'Questions? Call 1-555-123-4567',
+//   body: [
+//     'FAQ',
+//     'Account',
+//     'Investor Relations',
+//     'Redeem Gift Cards',
+//     'Ways to Watch',
+//     'Privacy',
+//     'Corporate Information',
+//     'Speed Test',
+//     'Only on Netflix',
+//     'Help Center',
+//     'Media Center',
+//     'Jobs',
+//     'Buy Gift Cards',
+//     'Terms of Use',
+//     'Cookie Preferences',
+//     'Contact Us',
+//     'Legal Notices',
+//   ],
+// }
