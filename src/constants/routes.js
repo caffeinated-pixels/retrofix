083b211d8ce2f4df8e66a4cf27cd99432af6ee5f
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-12 10:02:52 -0500

    Add new routes to constants

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,11 @@
 export const HOME = '/'
 export const BROWSE = '/browse'
+
 export const SIGN_UP = '/signup'
+export const REGISTRATION = '/signup/registration'
+export const REG_FORM = '/signup/regform'
+export const PLAN_FORM = '/signup/planform'
+
 export const SIGN_IN = '/signin'
+
 export const PROFILE = '/profile'
