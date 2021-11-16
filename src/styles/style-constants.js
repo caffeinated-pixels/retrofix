aef7e062e6d7e9c6093a7c1c4d00fd072c87f1e5
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-16 11:59:14 -0500

    Add bg colors to constants

--- src/styles/style-constants.js
+++ src/styles/style-constants.js
@@ -1,7 +1,10 @@
 export const colors = {
   netflixRed: `hsl(357, 92%, 47%)`,
   netflixRedFocus: `hsl(357, 92%, 52%)`,
+  bgWhite: `hsl(0, 0%, 100%)`,
+  bgBlack: 'hsl(0, 0%, 0%)',
   textMedGrey: 'hsl(0, 0%, 46%)',
+  textDarkGrey: 'hsl(0, 0%, 20%)',
   accordionGrey: 'hsl(0, 0%, 19%)',
 }
 
