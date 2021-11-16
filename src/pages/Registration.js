c8a4cfb921f40e7663c71b5926df52979aec21c0
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-16 13:16:28 -0500

    Create separate RegNavBar for registration page

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,4 +1,4 @@
-import { PageContainer, Navbar } from '../parts'
+import { PageContainer, RegNavbar } from '../parts'
 import { SiteLogo, NavLink } from '../components'
 import { SIGN_IN } from '../constants/routes'
 import { colors } from '../styles/style-constants'
@@ -6,17 +6,24 @@ import { colors } from '../styles/style-constants'
 export default function Registration() {
   return (
     <PageContainer bgColor={colors.bgWhite} txtColor={colors.textDarkGrey}>
-      <Navbar>
+      <RegNavbar>
         <SiteLogo />
         <NavLink href={SIGN_IN}>Sign In</NavLink>
-      </Navbar>
+      </RegNavbar>
     </PageContainer>
   )
 }
 
 /* 
 header
+  logo
+  Sign In link
+
 main
+  image
+  text
+  buttion
+
 footer
 
 
---
+++ src/parts/RegNavbar.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const NavContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 1.75em;
+  margin: 0 5%;
+  max-width: 1920px;
+
+  @media (min-width: 550px) {
+    margin: 0 45px;
+  }
+`
+export default function Navbar({ children, ...restProps }) {
+  return <NavContainer {...restProps}>{children}</NavContainer>
+}
