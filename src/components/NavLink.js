b543b8449492c9237c3d0479b231a7c06428876b
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-22 13:33:51 -0500

    Increase letter-spacing

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const Link = styled.a`
   text-decoration: none;
   font-size: 0.875rem;
+  letter-spacing: 0.2px;
   font-weight: 700;
   line-height: 3.2;
   margin: 0 10px;
