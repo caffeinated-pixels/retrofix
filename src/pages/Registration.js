5ae9f9d686d509c86905e3b4cb7789323cede192
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-22 15:43:52 -0500

    Remove commented out styles

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -15,21 +15,6 @@ const LogoWrapper = styled.div`
     width: 167px;
     height: 45px;
   }
-
-  /* @media (min-width: 550px) {
-    width: 6.75rem;
-    height: 2rem;
-  }
-
-  @media (min-width: 950px) {
-    width: 8.375rem;
-    height: 2.25rem;
-  }
-
-  @media (min-width: 1450px) {
-    width: 10.4375rem;
-    height: 2.8125rem;
-  } */
 `
 
 const LogoLink = styled.a``
