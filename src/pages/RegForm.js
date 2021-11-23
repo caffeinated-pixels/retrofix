34755cebf23172b2e372e5aaa020da2782668349
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-23 14:04:51 -0500

    Add form and inputs

--- src/pages/RegForm.js
+++ src/pages/RegForm.js
@@ -12,18 +12,64 @@ import { colors } from '../styles/style-constants'
 import { footerHomeRegistration } from '../fixtures/footer-content'
 import { PLAN_FORM } from '../constants/routes'
 
+const RegFormContainer = styled.div``
+
+const GeneralForm = styled.form``
+
+const RegFormText = styled.p`
+  font-size: 1.1875rem;
+`
+const FormInput = styled.input``
+
+const FormLabel = styled.label`
+  border: 0;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+`
+
 export default function RegForm() {
   return (
     <PageContainer bgColor={colors.bgWhite} txtColor={colors.textDarkGrey}>
       <RegNavbar />
       <RegContentContainer>
-        <StepHeaderContainer>
-          <StepIndicator currentStep='2' />
-          <StepTitle>Create a password to start your membership</StepTitle>
-        </StepHeaderContainer>
-        <p>Just a few more steps and you're done!</p>
-        <p>We hate paperwork, too.</p>
-        <SubmitButton route={PLAN_FORM}>Next</SubmitButton>
+        <RegFormContainer>
+          <StepHeaderContainer>
+            <StepIndicator currentStep='1' />
+            <StepTitle>Create a password to start your membership</StepTitle>
+          </StepHeaderContainer>
+          <GeneralForm>
+            <RegFormText>Just a few more steps and you're done!</RegFormText>
+            <RegFormText>We hate paperwork, too.</RegFormText>
+
+            <FormInput
+              id='name-input'
+              type='text'
+              placeholder='First Name'
+            ></FormInput>
+            <FormLabel htmlFor='name-input'>First Name</FormLabel>
+
+            <FormInput
+              id='email-input'
+              type='email'
+              placeholder='Email'
+            ></FormInput>
+            <FormLabel htmlFor='email-input'>Email Address</FormLabel>
+
+            <FormInput
+              id='password-input'
+              type='password'
+              placeholder='Password'
+            ></FormInput>
+            <FormLabel htmlFor='password-input'>Password</FormLabel>
+
+            <SubmitButton route={PLAN_FORM}>Next</SubmitButton>
+          </GeneralForm>
+        </RegFormContainer>
       </RegContentContainer>
       <Footer
         footerContent={footerHomeRegistration}
@@ -33,3 +79,20 @@ export default function RegForm() {
     </PageContainer>
   )
 }
+
+/* 
+PageContainer
+  RegNavbar
+  RegContentContainer (centerContainer)
+    RegForm
+      firstname
+      email
+      password
+      checkbox
+      submit button
+
+  Footer
+
+
+
+*/
