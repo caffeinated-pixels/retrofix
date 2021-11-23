f96d775e69e8244e222cf1b90d7cdd1ee0678e6f
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-23 09:50:38 -0500

    Move Route children to element prop

--- src/App.js
+++ src/App.js
@@ -15,30 +15,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route path={ROUTES.BROWSE}>
-          <Browse />
-        </Route>
-        <Route path={ROUTES.SIGN_IN}>
-          <Signin />
-        </Route>
-        <Route exact path={ROUTES.SIGN_UP}>
-          <Signup />
-        </Route>
-        <Route path={ROUTES.REGISTRATION}>
-          <Registration />
-        </Route>
-        <Route path={ROUTES.REG_FORM}>
-          <RegForm />
-        </Route>
-        <Route path={ROUTES.PLAN_FORM}>
-          <PlanForm />
-        </Route>
-        <Route path={ROUTES.PROFILE}>
-          <Profile />
-        </Route>
+        <Route exact path='/' element={<Home />} />
+        <Route path={ROUTES.BROWSE} element={<Browse />} />
+        <Route path={ROUTES.SIGN_IN} element={<Signin />} />
+        <Route exact path={ROUTES.SIGN_UP} element={<Signup />} />
+        <Route path={ROUTES.REGISTRATION} element={<Registration />} />
+        <Route path={ROUTES.REG_FORM} element={<RegForm />} />
+        <Route path={ROUTES.PLAN_FORM} element={<PlanForm />} />
+        <Route path={ROUTES.PROFILE} element={<Profile />} />
       </Routes>
     </Router>
   )
