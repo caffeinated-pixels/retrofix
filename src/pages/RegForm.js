9a389c4c344fbd7ff975e6990ac31a3ee9aea11b
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-23 12:00:27 -0500

    Build out RegForm skeleton

--- src/pages/RegForm.js
+++ src/pages/RegForm.js
@@ -1,3 +1,35 @@
+import styled from 'styled-components'
+import {
+  PageContainer,
+  RegNavbar,
+  RegContentContainer,
+  StepHeaderContainer,
+  Footer,
+} from '../parts'
+import { StepIndicator, StepTitle, SubmitButton } from '../components'
+
+import { colors } from '../styles/style-constants'
+import { footerHomeRegistration } from '../fixtures/footer-content'
+import { PLAN_FORM } from '../constants/routes'
+
 export default function RegForm() {
-  return <h1>RegForm page!</h1>
+  return (
+    <PageContainer bgColor={colors.bgWhite} txtColor={colors.textDarkGrey}>
+      <RegNavbar />
+      <RegContentContainer>
+        <StepHeaderContainer>
+          <StepIndicator currentStep='2' />
+          <StepTitle>Create a password to start your membership</StepTitle>
+        </StepHeaderContainer>
+        <p>Just a few more steps and you're done!</p>
+        <p>We hate paperwork, too.</p>
+        <SubmitButton route={PLAN_FORM}>Next</SubmitButton>
+      </RegContentContainer>
+      <Footer
+        footerContent={footerHomeRegistration}
+        bgColor={colors.bgLightGrey}
+        borderTop={true}
+      />
+    </PageContainer>
+  )
 }
