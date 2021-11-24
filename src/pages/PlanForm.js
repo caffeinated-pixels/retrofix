c920f937b5acab30bc8b239139a87f69fdec1094
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-24 14:58:24 -0500

    Add initial content to PlanForm

--- src/pages/PlanForm.js
+++ src/pages/PlanForm.js
@@ -1,3 +1,46 @@
-export default function PlanForm() {
-  return <h1>Planform page</h1>
+import { RegNavbar, Footer } from '../parts'
+import {
+  PageContainer,
+  StepHeaderContainer,
+  RegContentContainer,
+  RegContainer,
+  RegContextBody,
+} from '../containers'
+import { StepIndicator, StepTitle, RegList, SubmitButton } from '../components'
+import { footerHomeRegistration } from '../fixtures/footer-content'
+import { PLAN_FORM } from '../constants/routes'
+import { colors } from '../styles/style-constants'
+
+export default function Registration() {
+  return (
+    <PageContainer bgColor={colors.bgWhite} txtColor={colors.textDarkGrey}>
+      <RegNavbar />
+      <RegContentContainer>
+        <RegContainer>
+          <StepHeaderContainer>
+            <StepIndicator currentStep='2' />
+            <StepTitle>Choose the plan that’s right for you</StepTitle>
+          </StepHeaderContainer>
+          <RegContextBody>
+            <RegList planform>
+              <RegList.Item planform>Watch all you want. Ad-free.</RegList.Item>
+              <RegList.Item planform>
+                Recommendations just for you.
+              </RegList.Item>
+              <RegList.Item planform>
+                Change or cancel your plan anytime.
+              </RegList.Item>
+            </RegList>
+          </RegContextBody>
+        </RegContainer>
+
+        <SubmitButton route={PLAN_FORM}>Next</SubmitButton>
+      </RegContentContainer>
+      <Footer
+        footerContent={footerHomeRegistration}
+        bgColor={colors.bgLightGrey}
+        borderTop={true}
+      />
+    </PageContainer>
+  )
 }
