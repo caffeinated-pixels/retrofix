eeddc7490063c53ef7343cbf931e20f6041be5e3
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-24 12:03:08 -0500

    Create skeleton for Signup page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,3 +1,55 @@
-export default function Signin() {
-  return <h1>I am the Signup Page</h1>
+import styled from 'styled-components'
+import {
+  PageContainer,
+  RegNavbar,
+  RegContentContainer,
+  StepHeaderContainer,
+  Footer,
+} from '../parts'
+import {
+  StepLogoContainer,
+  StepIndicator,
+  StepTitle,
+  RegParagraph,
+  SubmitButton,
+} from '../components'
+import { footerHomeRegistration } from '../fixtures/footer-content'
+import { REG_FORM } from '../constants/routes'
+import { colors } from '../styles/style-constants'
+
+const RegContainer = styled.div`
+  max-width: 340px;
+  margin: 0 auto 0;
+
+  @media (min-width: 600px) {
+    text-align: center;
+  }
+`
+
+export default function Registration() {
+  return (
+    <PageContainer bgColor={colors.bgWhite} txtColor={colors.textDarkGrey}>
+      <RegNavbar />
+      <RegContentContainer>
+        <RegContainer>
+          <StepLogoContainer bgImage='../images/misc/reg-checkmark-circle.png' />
+          <StepHeaderContainer>
+            <StepIndicator currentStep='2' />
+            <StepTitle>Choose your plan.</StepTitle>
+          </StepHeaderContainer>
+          <RegParagraph>
+            Netflix is personalized for you. Create a password to watch on any
+            device at any time.
+          </RegParagraph>
+        </RegContainer>
+
+        <SubmitButton route={REG_FORM}>Next</SubmitButton>
+      </RegContentContainer>
+      <Footer
+        footerContent={footerHomeRegistration}
+        bgColor={colors.bgLightGrey}
+        borderTop={true}
+      />
+    </PageContainer>
+  )
 }
