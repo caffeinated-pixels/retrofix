f6a65dedd03dd3944d37db29a92dbebf89772a00
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-26 11:09:41 -0500

    push local state to context if submit btn clicked

--- src/pages/RegForm.js
+++ src/pages/RegForm.js
@@ -37,6 +37,9 @@ export default function RegForm() {
   const navigate = useNavigate()
 
   const nextPage = () => {
+    setGlobalFirstName(firstName)
+    setGlobalEmail(email)
+    setGlobalPassword(password)
     navigate(SIGN_UP)
   }
 
