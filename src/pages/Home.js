20f4837d239dc3a9a5717166a02a2f7fcc8f6ca3
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-30 13:47:20 -0500

    Add SemanticHeaderWrapper

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,32 @@
+import styled from 'styled-components'
 import { Header, Navbar, StoryCards, FaqSection, Footer } from '../parts/'
 import { MainContainer } from '../containers/'
 import { HeaderFeature, SiteLogo, LinkButton } from '../components'
 import { footerHomeContent } from '../fixtures/footer-content'
 
+const SemanticHeader = styled.header``
+
 export default function Home() {
   return (
     <>
-      <Header hasBtn hasBorder>
-        <Header.ImageGradient>
-          <Navbar>
-            <Header.LogoWrapper>
-              <SiteLogo />
-            </Header.LogoWrapper>
-            <LinkButton href='/signin'>Sign In</LinkButton>
-          </Navbar>
-          <HeaderFeature />
-        </Header.ImageGradient>
-      </Header>
+      <SemanticHeader>
+        <Header hasBtn hasBorder>
+          <Header.ImageGradient>
+            <Navbar>
+              <Header.LogoWrapper>
+                <SiteLogo />
+              </Header.LogoWrapper>
+              <LinkButton href='/signin'>Sign In</LinkButton>
+            </Navbar>
+            <HeaderFeature />
+          </Header.ImageGradient>
+        </Header>
+      </SemanticHeader>
+
       <MainContainer>
         <StoryCards />
         <FaqSection />
       </MainContainer>
-
       <Footer footerContent={footerHomeContent} increasedPadding />
     </>
   )
