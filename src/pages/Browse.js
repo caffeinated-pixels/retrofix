88379de9f5263fb88ca55388abfd40e92e696455
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-30 15:55:26 -0500

    Setup very basic structure for Browse page

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,3 +1,39 @@
+import styled from 'styled-components'
+import { RegNavbar, Footer } from '../parts/'
+import { SemanticHeader, MainContainer } from '../containers/'
+import { footerHomeContent } from '../fixtures/footer-content'
+
+const BrowseHeader = styled.div``
+
 export default function Browse() {
-  return <h1>I am the Browse Page</h1>
+  return (
+    <>
+      <SemanticHeader>
+        <BrowseHeader>
+          <RegNavbar noBorder />
+        </BrowseHeader>
+      </SemanticHeader>
+
+      <MainContainer>
+        <h1>Carousels go here</h1>
+      </MainContainer>
+      <Footer footerContent={footerHomeContent} increasedPadding />
+    </>
+  )
 }
+
+/* 
+header (build from scratch)
+  navbar (build from scratch)
+  billboard (on larger devices)
+  my list carousel
+/header
+
+main
+  genre carousels
+/main
+
+footer
+
+
+*/
