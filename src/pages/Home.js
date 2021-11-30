c4808237e21e57edc572f1c6afc0fd3b7dda61df
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-11-30 15:45:25 -0500

    Remove redundant hasBtn prop from Header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <SemanticHeader>
-        <Header hasBtn hasBorder>
+        <Header hasBorder>
           <Header.ImageGradient>
             <Navbar>
               <Header.LogoWrapper>
