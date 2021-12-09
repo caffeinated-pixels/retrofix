2839dfb6df557a76d26047e7482354a7c903280f
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-12-09 10:49:10 -0500

    Add navSliderBorderBottom

--- src/styles/style-constants.js
+++ src/styles/style-constants.js
@@ -14,5 +14,6 @@ export const colors = {
 }
 
 export const cardBorderBottom = '8px solid #222'
+export const navSliderBorderBottom = '1px solid #333'
 
 export const focusOutline = '2px solid yellow'
