a03806d98aea4f14197ce96fcb84576ab047ef50
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-12-09 10:06:32 -0500

    Wrap App in FirebaseAuthContextProvider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/scss/main.scss'
 import App from './App'
+import FirebaseAuthContextProvider from './context/FirebaseAuthContext'
 import SignUpContextProvider from './context/SignUpContext'
 
 ReactDOM.render(
-  <SignUpContextProvider>
-    <App />
-  </SignUpContextProvider>,
-
+  <FirebaseAuthContextProvider>
+    <SignUpContextProvider>
+      <App />
+    </SignUpContextProvider>
+  </FirebaseAuthContextProvider>,
   document.getElementById('root')
 )
