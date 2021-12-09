9ff6a80d222246e9c66cbd8b2156374f8e9ee11d
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-12-09 09:53:31 -0500

    Create FirebaseAuthContext

---
+++ src/context/FirebaseAuthContext.js
@@ -0,0 +1,19 @@
+import { createContext, useState } from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { firebaseAuth } from '../firebase/config'
+
+export const FirebaseAuthContext = createContext()
+
+export default function FirebaseAuthContextProvider({ children }) {
+  const [user, setUser] = useState()
+
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    setUser(currentUser)
+  })
+
+  return (
+    <FirebaseAuthContext.Provider value={{ user }}>
+      {children}
+    </FirebaseAuthContext.Provider>
+  )
+}
