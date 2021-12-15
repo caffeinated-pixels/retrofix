525c1fabe04fa22cbea3cc7a9e17931ac8801a5b
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-12-15 15:11:26 -0500

    Add email validation function to helpers

---
+++ src/helpers/validate-email.js
@@ -0,0 +1,7 @@
+export default function isEmailValid(email) {
+  const emailRegex =
+    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+  // RFC2822 Email validation from https://regexr.com/
+
+  return emailRegex.test(email)
+}
