eba5531df8fc703098eaac48e980fcd6c9155d85
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-01-07 15:52:07 -0500

    Remove h1 from BrowseDesktopLayout

--- src/parts/BrowseHeaderDesktop.js
+++ src/parts/BrowseHeaderDesktop.js
@@ -13,9 +13,35 @@ export default function BrowseHeaderDesktop() {
             <SiteLogo />
           </BrowseHeader.LogoWrapperDesktop>
 
-          <BrowseHeader.SearchForm>
-            <BrowseHeader.SearchInput placeholder='Search' />
-          </BrowseHeader.SearchForm>
+          <BrowseHeader.NavPrimary>
+            <BrowseHeader.NavPrimaryItem>
+              <BrowseHeader.NavPrimaryBtn>Home</BrowseHeader.NavPrimaryBtn>
+            </BrowseHeader.NavPrimaryItem>
+
+            <BrowseHeader.NavPrimaryItem>
+              <BrowseHeader.NavPrimaryBtn>Films</BrowseHeader.NavPrimaryBtn>
+            </BrowseHeader.NavPrimaryItem>
+
+            <BrowseHeader.NavPrimaryItem>
+              <BrowseHeader.NavPrimaryBtn>Series</BrowseHeader.NavPrimaryBtn>
+            </BrowseHeader.NavPrimaryItem>
+          </BrowseHeader.NavPrimary>
+
+          <BrowseHeader.NavSecondary>
+            <BrowseHeader.NavSecondaryItem>
+              <BrowseHeader.SearchForm>
+                <BrowseHeader.SearchInput placeholder='Search' />
+              </BrowseHeader.SearchForm>
+            </BrowseHeader.NavSecondaryItem>
+
+            <BrowseHeader.NavSecondaryItem>
+              <BrowseHeader.BellIcon />
+            </BrowseHeader.NavSecondaryItem>
+
+            <BrowseHeader.NavSecondaryItem>
+              <BrowseHeader.Avatar src='./images/users/1.png' />
+            </BrowseHeader.NavSecondaryItem>
+          </BrowseHeader.NavSecondary>
         </BrowseHeader.Navbar>
       </BrowseHeader>
     </SemanticHeader>
