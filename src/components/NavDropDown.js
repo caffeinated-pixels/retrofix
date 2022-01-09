a36f83b37f97441576cfa8a9ee0a95b8a7482022
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-01-09 07:38:04 -0500

    Add DropDownIcon

--- src/components/NavDropDown.js
+++ src/components/NavDropDown.js
@@ -1,17 +1,24 @@
 import styled from 'styled-components'
 
-const DropDownWrapper = styled.div``
+const DropDownWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`
 
 const Avatar = styled.img`
   width: 32px;
   height: 32px;
   border-radius: 4px;
 `
+const ChevronIcon = styled.i`
+  margin-left: 10px;
+`
 
 export default function NavDropDown() {
   return (
     <DropDownWrapper>
       <Avatar src='./images/users/1.png' />
+      <ChevronIcon className='fas fa-caret-up' />
     </DropDownWrapper>
   )
 }
