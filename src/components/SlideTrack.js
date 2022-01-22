f257418e75831d7c78668327bd2d350f7758f0ba
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-01-22 12:13:53 -0500

    Create seperate SlideTrack component

---
+++ src/components/SlideTrack.js
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+import { ContentBox } from '../components'
+
+const SlideWrapper = styled.div`
+  position: relative;
+`
+
+const GoBackBox = styled.div`
+  position: absolute;
+  width: 4%;
+  top: 0;
+  bottom: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  background: rgba(20, 20, 20, 0.5);
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+
+  &:hover {
+    background: rgba(20, 20, 20, 0.7);
+  }
+`
+const GoForwardBox = styled(GoBackBox)`
+  right: 0;
+`
+
+const ArrowIcon = styled.i`
+  font-size: 3rem;
+`
+
+const GenreRow = styled.div`
+  display: flex;
+  padding-left: 4%;
+`
+
+export default function SlideTrack({ content }) {
+  const [slideOffset, setSlideOffset] = useState('0px')
+
+  return (
+    <SlideWrapper>
+      <GoBackBox className='go-back'>
+        <ArrowIcon className='fas fa-angle-left' />
+      </GoBackBox>
+      <GenreRow offset={slideOffset}>
+        {content.map((item) => (
+          <ContentBox key={item.title} item={item} />
+        ))}
+      </GenreRow>
+      <GoForwardBox className='go-forward'>
+        <ArrowIcon className='fas fa-angle-right' />
+      </GoForwardBox>
+    </SlideWrapper>
+  )
+}
--- src/parts/StreamingContentDesktop.js
+++ src/parts/StreamingContentDesktop.js
@@ -2,7 +2,7 @@ import { useContext, useMemo } from 'react'
 import styled from 'styled-components'
 import { BrowseContext } from '../context/BrowseContext'
 import { MainContainer } from '../containers/'
-import { ContentBox } from '../components'
+import { SlideTrack } from '../components'
 import sortStreamingContent from '../helpers/sort-streaming-content'
 
 const GenreContainersWrapper = styled.div`
@@ -21,41 +21,6 @@ const GenreTitle = styled.h2`
   margin-bottom: 0.5em;
 `
 
-const SlideWrapper = styled.div`
-  position: relative;
-`
-
-const GoBackBox = styled.div`
-  position: absolute;
-  width: 4%;
-  top: 0;
-  bottom: 0;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-
-  background: rgba(20, 20, 20, 0.5);
-  border-top-left-radius: 4px;
-  border-bottom-left-radius: 4px;
-
-  &:hover {
-    background: rgba(20, 20, 20, 0.7);
-  }
-`
-const GoForwardBox = styled(GoBackBox)`
-  right: 0;
-`
-
-const ArrowIcon = styled.i`
-  font-size: 3rem;
-`
-
-const GenreRow = styled.div`
-  display: flex;
-  padding-left: 4%;
-`
 export default function StreamingContentDesktop() {
   const { activeCategory, unsortedStreamingContent } = useContext(BrowseContext)
 
@@ -68,20 +33,7 @@ export default function StreamingContentDesktop() {
     return sortedStreamingContent.map(({ genre, content }) => (
       <GenreContainer key={genre}>
         <GenreTitle>{genre}</GenreTitle>
-
-        <SlideWrapper>
-          <GoBackBox className='go-back'>
-            <ArrowIcon className='fas fa-angle-left' />
-          </GoBackBox>
-          <GenreRow>
-            {content.map((item) => (
-              <ContentBox key={item.title} item={item} />
-            ))}
-          </GenreRow>
-          <GoForwardBox className='go-forward'>
-            <ArrowIcon className='fas fa-angle-right' />
-          </GoForwardBox>
-        </SlideWrapper>
+        <SlideTrack content={content} />
       </GenreContainer>
     ))
   }, [activeCategory, unsortedStreamingContent])
