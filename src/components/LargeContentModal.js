3cac8ba6f852f3e11898c10fff60a4b452d22e80
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-01-25 16:03:52 -0500

    Add bg img to header section of modal

--- src/components/LargeContentModal.js
+++ src/components/LargeContentModal.js
@@ -4,23 +4,52 @@ import { colors } from '../styles/style-constants'
 
 const modalRoot = document.getElementById('modal-root')
 
-const Container = styled.div`
-  background-color: grey;
+const Background = styled.div`
+  background-color: hsla(0, 0%, 0%, 0.7);
+
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const ModalContainer = styled.div`
   background-color: ${colors.bgBrowseDarkGrey};
-
-  position: absolute;
-  width: 80%;
-  height: 374px;
-
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
+  background-color: #181818;
+  width: min(90%, 950px);
+  height: 80%;
   border-radius: 6px;
 `
 
-export default function LargeContentModal({ handleCloseModal }) {
+const Header = styled.div`
+  position: relative;
+  height: 70%;
+  border-top-left-radius: 6px;
+  border-top-right-radius: 6px;
+
+  background-image: url(${({ imgUrl }) => imgUrl});
+  background-size: cover;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(to top, #181818, transparent 50%);
+  }
+`
+export default function LargeContentModal({ handleCloseModal, imgUrl }) {
   return ReactDOM.createPortal(
-    <Container onClick={handleCloseModal}></Container>,
+    <Background onClick={handleCloseModal}>
+      <ModalContainer>
+        <Header imgUrl={imgUrl}></Header>
+      </ModalContainer>
+    </Background>,
     modalRoot
   )
 }
