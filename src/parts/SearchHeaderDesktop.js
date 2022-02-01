57dbdb708a1fffd6d6f1d499af54c43a78c01a27
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-02-01 10:52:28 -0500

    Add basic content to SearchHeaderDesktop

--- src/parts/SearchHeaderDesktop.js
+++ src/parts/SearchHeaderDesktop.js
@@ -1,3 +1,52 @@
+import { BrowseHeader, SiteLogo, NavDropDown } from '../components'
+import { SemanticHeader } from '../containers'
+
 export default function SearchHeaderDesktop() {
-  return <h1>I am the SearchHeaderDesktop</h1>
+  return (
+    <SemanticHeader>
+      <BrowseHeader>
+        <BrowseHeader.Navbar padding='0 4vw'>
+          <BrowseHeader.LogoWrapperDesktop>
+            <SiteLogo />
+          </BrowseHeader.LogoWrapperDesktop>
+
+          <BrowseHeader.NavPrimary>
+            <BrowseHeader.NavPrimaryItem>
+              <BrowseHeader.NavPrimaryBtn>Home</BrowseHeader.NavPrimaryBtn>
+            </BrowseHeader.NavPrimaryItem>
+
+            <BrowseHeader.NavPrimaryItem>
+              <BrowseHeader.NavPrimaryBtn>Films</BrowseHeader.NavPrimaryBtn>
+            </BrowseHeader.NavPrimaryItem>
+
+            <BrowseHeader.NavPrimaryItem>
+              <BrowseHeader.NavPrimaryBtn>Series</BrowseHeader.NavPrimaryBtn>
+            </BrowseHeader.NavPrimaryItem>
+          </BrowseHeader.NavPrimary>
+
+          <BrowseHeader.NavSecondary>
+            <BrowseHeader.NavSecondaryItem>
+              <BrowseHeader.SearchForm>
+                <BrowseHeader.SearchWrapper isSearchOpen={true}>
+                  <BrowseHeader.SearchIcon />
+                  <BrowseHeader.SearchInputDesktop
+                    isSearchOpen={true}
+                    placeholder='Search'
+                  />
+                </BrowseHeader.SearchWrapper>
+              </BrowseHeader.SearchForm>
+            </BrowseHeader.NavSecondaryItem>
+
+            <BrowseHeader.NavSecondaryItem>
+              <BrowseHeader.BellIcon />
+            </BrowseHeader.NavSecondaryItem>
+
+            <BrowseHeader.NavSecondaryItem>
+              <NavDropDown />
+            </BrowseHeader.NavSecondaryItem>
+          </BrowseHeader.NavSecondary>
+        </BrowseHeader.Navbar>
+      </BrowseHeader>
+    </SemanticHeader>
+  )
 }
