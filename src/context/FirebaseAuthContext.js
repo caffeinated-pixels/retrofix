8ecc8e721ef1a364de1c780d2a6e40b8e21a4c83
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-02-02 17:12:35 -0500

    RF to place currentUser into localStorage
    It seems that when a user types in a URL (as opposed to redirecting with
    useNavigate), the entire React DOM tree is rebuilt! This mean that
    initally there is no user stored in the Context state until firebase can
    be called again. This was causing me problems with protected routes, as
    conditionals checking for a user will evaluate to false on the first
    render (but not later renders, by which it's too late!).
    
    The way to circumvent this is to place the user in localStorage and
    initialize Context state with this value. We can then update both
    localStorage and state appropriately using Firebase's onAuthStateChanged
    listener. Fun!

--- src/context/FirebaseAuthContext.js
+++ src/context/FirebaseAuthContext.js
@@ -3,13 +3,20 @@ import { onAuthStateChanged } from 'firebase/auth'
 import { firebaseAuth } from '../firebase/config'
 
 const FirebaseAuthContext = createContext()
+const retroFixUser = JSON.parse(localStorage.getItem('retroFixUser'))
 
 export default function FirebaseAuthContextProvider({ children }) {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(retroFixUser)
 
   useEffect(() => {
     const authListener = onAuthStateChanged(firebaseAuth, (currentUser) => {
-      setUser(currentUser)
+      if (currentUser) {
+        localStorage.setItem('retroFixUser', JSON.stringify(currentUser))
+        setUser(currentUser)
+      } else {
+        localStorage.removeItem('retroFixUser')
+        setUser(null)
+      }
     })
 
     return authListener
