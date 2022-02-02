e5f6187f5f71a04c9da4012d017016bfe24bba87
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-02-02 09:41:36 -0500

    Synce searchInput with URL query param

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 import Fuse from 'fuse.js'
 import useWindowWidth from '../hooks/useWindowWidth'
 import { BrowsePageContainer } from '../containers'
@@ -10,20 +11,36 @@ import {
 } from '../parts/'
 import { footerHomeContent } from '../fixtures/footer-content'
 import unsortedStreamingContent from '../fixtures/shows-and-films.json'
+import { BROWSE } from '../constants/routes'
 
 export default function SearchPage() {
-  const [searchInput, setSearchInput] = useState('RoboCop')
+  const [searchInput, setSearchInput] = useState('')
   const [searchResults, setSearchResults] = useState([])
+  const navigate = useNavigate()
+  const { search } = useLocation()
   const width = useWindowWidth()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('submit')
   }
 
   const handleSearchInput = (e) => {
     setSearchInput(e.target.value)
+
+    if (e.target.value.trim() === '') {
+      // return to browse page if searchInput becomes empty
+      navigate(BROWSE)
+    } else {
+      // sync URL param and searchInput
+      navigate(`?q=${e.target.value}`)
+    }
   }
+
+  useEffect(() => {
+    const searchParam = new URLSearchParams(search)
+    // initialize searchInput with URL query paramater (set by Browse page)
+    if (searchParam.get('q')) setSearchInput(searchParam.get('q'))
+  }, [search])
 
   useEffect(() => {
     const fuse = new Fuse(unsortedStreamingContent, {
