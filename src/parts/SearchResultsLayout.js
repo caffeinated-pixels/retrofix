0d826969b4199f85b8010327c3846a32f8a60adf
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-02-02 12:55:41 -0500

    SearchResults now call maps to ContentSlide comp

--- src/parts/SearchResultsLayout.js
+++ src/parts/SearchResultsLayout.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { ContentSlide } from '../components'
 
 const LayoutContainer = styled.div`
   padding: 70px 4vw 0;
@@ -7,38 +8,12 @@ const LayoutContainer = styled.div`
 const ResultsGallery = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(200px, auto));
-  grid-column-gap: 0.4vw;
   grid-row-gap: 48px;
-`
-
-const Container = styled.div`
-  cursor: pointer;
-
-  transition: all 250ms;
-`
-const ContentImage = styled.img`
-  object-fit: cover;
-
-  &:hover,
-  &:focus-visible {
-    transform: scale(0.98);
-    filter: brightness(0.5);
-  }
-
-  &:focus-visible {
-    outline: auto;
-  }
 `
 
 export default function SearchResultsLayout({ searchResults }) {
   const results = searchResults.map((show, i) => {
-    const imgUrl = `/images/${show.category}/${show.genre}/${show.slug}/thumb.jpg`
-
-    return (
-      <Container key={`result-${i}`}>
-        <ContentImage tabIndex='0' src={imgUrl} alt={show.title} />
-      </Container>
-    )
+    return <ContentSlide key={`result-${i}`} item={show} />
   })
 
   return (
