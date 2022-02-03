23b9255d2434305b967263824fdd91d51109a2ce
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-02-03 12:25:51 -0500

    RF SIGN_UP into nested route

--- src/App.js
+++ src/App.js
@@ -23,10 +23,14 @@ function App() {
           <Route path={ROUTES.BROWSE} element={<Browse />} />
         </Route>
         <Route path={ROUTES.SIGN_IN} element={<Signin />} />
-        <Route path={ROUTES.SIGN_UP} element={<Signup />} />
-        <Route path={ROUTES.REGISTRATION} element={<Registration />} />
-        <Route path={ROUTES.REG_FORM} element={<RegForm />} />
-        <Route path={ROUTES.PLAN_FORM} element={<PlanForm />} />
+
+        <Route path={ROUTES.SIGN_UP}>
+          <Route index element={<Signup />}></Route>
+          <Route path={ROUTES.REGISTRATION} element={<Registration />} />
+          <Route path={ROUTES.REG_FORM} element={<RegForm />} />
+          <Route path={ROUTES.PLAN_FORM} element={<PlanForm />} />
+        </Route>
+
         <Route path={ROUTES.PROFILE} element={<Profile />} />
         <Route path={ROUTES.GET_THE_APP} element={<GetTheApp />} />
         <Route path={ROUTES.SEARCH} element={<SearchPage />} />
