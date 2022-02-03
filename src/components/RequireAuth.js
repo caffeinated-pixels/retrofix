a7fa6cea727452de86e32c2ccfa3373626db2b57
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-02-03 13:13:19 -0500

    RF RequireAuth, add replace attr to Navigate

--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -1,12 +1,10 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuth } from '../context/FirebaseAuthContext'
+import { SIGN_IN } from '../constants/routes'
 
 export default function RequireAuth() {
   const user = useAuth()
 
-  if (!user) {
-    return <Navigate to='/signin' />
-  }
-
-  return <Outlet />
+  return user ? <Outlet /> : <Navigate to={SIGN_IN} replace />
+  // replace attr allows user to return to the page they came from using the browser back button
 }
