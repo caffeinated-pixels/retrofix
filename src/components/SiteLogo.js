7ec0a507a397f1057b392e74ef5da7e44d7a0327
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-02-03 13:46:47 -0500

    Add Home page link to site logo

--- src/components/SiteLogo.js
+++ src/components/SiteLogo.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { HOME } from '../constants/routes'
 
 const Logo = styled.img``
 
 export default function SiteLogo({ ...restProps }) {
   return (
-    <Logo src='../images/misc/logo.svg' alt='Netflix logo' {...restProps} />
+    <Link to={HOME}>
+      <Logo src='../images/misc/logo.svg' alt='Netflix logo' {...restProps} />
+    </Link>
   )
 }
