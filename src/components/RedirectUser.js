ab6c139e2a1fb1f608d47766e5c9b41ff22fde29
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-02-04 10:22:23 -0500

    Fix blank profile immediately after FB reg
    There was an issue with the avatar image and user's name not appearing
    on the profile page immediately after registration. This is because
    the displayName and photoURL are not added to the user until after the
    user has been created.
    
    So, RedirectUser fires because the FB user object
    updates and redirects to the profile page and does not wait for
    updateProfile to complete. onAuthStateChanged only fires when the user
    status changes; it does not fire for profile updates. So, the user will
    only update on a page refresh.
    
    So, I've added an extra conditional to RedirectUser. If the route that
    triggered the redirect was the planform and there is no displayName, it
    cancels the redirect. This allows planform page to wait until
    updateProfile completes before navigating to the profile page.

--- src/components/RedirectUser.js
+++ src/components/RedirectUser.js
@@ -1,9 +1,14 @@
-import { Navigate, Outlet } from 'react-router-dom'
+import { useLocation, Navigate, Outlet } from 'react-router-dom'
 import { useAuth } from '../context/FirebaseAuthContext'
 import { PROFILE } from '../constants/routes'
 
 export default function RedirectUser() {
   const user = useAuth()
+  const location = useLocation()
+
+  if (location.pathname === '/signup/planform') {
+    return user?.displayName ? <Navigate to={PROFILE} /> : <Outlet />
+  } // this prevents an issue immediately after registration, where the user object from Firebase doesn't yet contain displayName and photoURL properties (which results in a blank profile until refresh). It prevents redirection, allowing the planform page to wait until updateProfile completes before navigating to the profile page.
 
   return user ? <Navigate to={PROFILE} /> : <Outlet />
   // if user is logged in, send them to the profile page like on Netflix
