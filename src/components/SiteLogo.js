64311d1ed96ca72e96b0a392c20805b1fe440399
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-02-04 13:09:08 -0500

    Change color of Retrofix logo to blue

--- src/components/SiteLogo.js
+++ src/components/SiteLogo.js
@@ -2,7 +2,10 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { HOME } from '../constants/routes'
 
-const Logo = styled.img``
+const Logo = styled.img`
+  filter: invert(42%) sepia(79%) saturate(2023%) hue-rotate(164deg)
+    brightness(98%) contrast(103%);
+`
 
 export default function SiteLogo({ ...restProps }) {
   return (
