160046beeee74129f317141edb892690f1058029
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-02-07 10:48:24 -0500

    Add chaos-engine video & poster to Watch page

--- src/pages/Watch.js
+++ src/pages/Watch.js
@@ -1,3 +1,23 @@
+import styled from 'styled-components'
+
+const VideoPlayer = styled.video`
+  display: block;
+  margin: 0 auto;
+  width: 100vw;
+  height: 100vh;
+`
+
 export default function Watch() {
-  return <h1>Watch page</h1>
+  return (
+    <>
+      <VideoPlayer
+        controls
+        src='/videos/chaos-engine.mp4'
+        poster='/images/misc/chaos-engine.png'
+        autoPlay
+      >
+        Sorry, your browser doesn't support embedded videos.
+      </VideoPlayer>
+    </>
+  )
 }
