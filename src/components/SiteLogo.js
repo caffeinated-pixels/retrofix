cdf3b6521b171f0c328b69d0a5ab489c3934956d
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-02-09 10:26:11 -0500

    Add focus-visible state to site logo

--- src/components/SiteLogo.js
+++ src/components/SiteLogo.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
 import { HOME } from '../constants/routes'
+
+const Link = styled(RouterLink)`
+  &:focus-visible {
+    outline: auto;
+  }
+`
 
 const Logo = styled.img`
   filter: invert(42%) sepia(79%) saturate(2023%) hue-rotate(164deg)
