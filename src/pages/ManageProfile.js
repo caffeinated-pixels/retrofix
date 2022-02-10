bb84c6ffa0feef2a7652198d324234f1955082b2
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-02-10 13:08:16 -0500

    RF ManageProfile to import comps from Profile

--- src/pages/ManageProfile.js
+++ src/pages/ManageProfile.js
@@ -1,24 +1,8 @@
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
 import { Header, Navbar } from '../parts/'
 import { SemanticHeader, PageContainer } from '../containers/'
-import { SiteLogo } from '../components'
-import { BROWSE, MANAGE_PROFILE } from '../constants/routes'
-import { colors } from '../styles/style-constants'
-
-const MainContainer = styled.main`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  flex-grow: 1;
-`
-
-const Title = styled.h1`
-  font-size: max(1.875rem, 3.5vw);
-  font-weight: 400;
-`
+import { SiteLogo, profile } from '../components'
+import { BROWSE } from '../constants/routes'
 
 const ProfileList = styled.ul`
   display: flex;
@@ -26,54 +10,6 @@ const ProfileList = styled.ul`
   justify-content: center;
   padding: 0 2.5em;
   gap: 2vw;
-`
-
-const ListItem = styled.li`
-  list-style: none;
-`
-const NavLink = styled(Link)`
-  color: ${colors.textLightGrey};
-  text-decoration: none;
-
-  &:hover {
-    color: #fff;
-  }
-
-  &:hover > div::after {
-    border-color: white;
-  }
-`
-
-const Avatar = styled.div`
-  position: relative;
-  background-color: ${colors.textDarkGrey};
-  background-image: ${({ imgUrl }) => (imgUrl ? `url(${imgUrl})` : '')};
-  background-size: cover;
-  height: clamp(84px, 10vw, 200px);
-  width: clamp(84px, 10vw, 200px);
-  border-radius: 4px;
-
-  &::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-
-    border-radius: 4px;
-    border: 2px solid transparent;
-  }
-`
-
-const ChooseAvatar = styled(Avatar)`
-  background-color: transparent;
-`
-
-const Name = styled.p`
-  margin: 0.6em 0;
-  text-align: center;
-  font-size: max(0.75rem, 1.3vw);
 `
 
 export default function ManageProfile() {
@@ -87,47 +23,47 @@ export default function ManageProfile() {
         </Navbar>
       </SemanticHeader>
 
-      <MainContainer>
-        <Title>Choose an avatar</Title>
+      <profile.MainContainer>
+        <profile.Title>Choose an avatar</profile.Title>
         <ProfileList>
-          <ListItem>
-            <NavLink to='#'>
-              <Avatar imgUrl='./images/users/1.png' />
-              <Name>Brigand</Name>
-            </NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink to='#'>
-              <ChooseAvatar imgUrl='./images/users/2.png' />
-              <Name>Gentleman</Name>
-            </NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink to='#'>
-              <ChooseAvatar imgUrl='./images/users/3.png' />
-              <Name>Mercenary</Name>
-            </NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink to='#'>
-              <ChooseAvatar imgUrl='./images/users/4.png' />
-              <Name>Navvie</Name>
-            </NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink to='#'>
-              <ChooseAvatar imgUrl='./images/users/5.png' />
-              <Name>Preacher</Name>
-            </NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink to='#'>
-              <ChooseAvatar imgUrl='./images/users/6.png' />
-              <Name>Thug</Name>
-            </NavLink>
-          </ListItem>
+          <profile.ListItem>
+            <profile.NavLink to='#'>
+              <profile.Avatar imgUrl='./images/users/1.png' />
+              <profile.Name>Brigand</profile.Name>
+            </profile.NavLink>
+          </profile.ListItem>
+          <profile.ListItem>
+            <profile.NavLink to='#'>
+              <profile.Avatar imgUrl='./images/users/2.png' />
+              <profile.Name>Gentleman</profile.Name>
+            </profile.NavLink>
+          </profile.ListItem>
+          <profile.ListItem>
+            <profile.NavLink to='#'>
+              <profile.Avatar imgUrl='./images/users/3.png' />
+              <profile.Name>Mercenary</profile.Name>
+            </profile.NavLink>
+          </profile.ListItem>
+          <profile.ListItem>
+            <profile.NavLink to='#'>
+              <profile.Avatar imgUrl='./images/users/4.png' />
+              <profile.Name>Navvie</profile.Name>
+            </profile.NavLink>
+          </profile.ListItem>
+          <profile.ListItem>
+            <profile.NavLink to='#'>
+              <profile.Avatar imgUrl='./images/users/5.png' />
+              <profile.Name>Preacher</profile.Name>
+            </profile.NavLink>
+          </profile.ListItem>
+          <profile.ListItem>
+            <profile.NavLink to='#'>
+              <profile.Avatar imgUrl='./images/users/6.png' />
+              <profile.Name>Thug</profile.Name>
+            </profile.NavLink>
+          </profile.ListItem>
         </ProfileList>
-      </MainContainer>
+      </profile.MainContainer>
     </PageContainer>
   )
 }
