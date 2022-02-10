ff4183f77e764c082bf7b14ab2b492f53c8aade6
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-02-10 07:13:43 -0500

    Create WindowWidthContext and provide to browse page

---
+++ src/context/WindowWidthContext.js
@@ -0,0 +1,26 @@
+import { useLayoutEffect, useState, createContext, useContext } from 'react'
+
+export const WindowWidthContext = createContext()
+
+export function WindowWidthContextProvider({ children }) {
+  const [width, setWidth] = useState(window.innerWidth)
+
+  // useLayoutEffect runs before React renders the component making it a better choice for DOM measurement than useEffect, which runs after rendering/screen painting
+  useLayoutEffect(() => {
+    const updateWidth = () => setWidth(window.innerWidth)
+
+    // updates state when screen width changes
+    window.addEventListener('resize', updateWidth)
+
+    // clean up event listener
+    return () => window.removeEventListener('resize', updateWidth)
+  }, [])
+
+  return (
+    <WindowWidthContext.Provider value={width}>
+      {children}
+    </WindowWidthContext.Provider>
+  )
+}
+
+export const useWindowWidthContext = () => useContext(WindowWidthContext)
